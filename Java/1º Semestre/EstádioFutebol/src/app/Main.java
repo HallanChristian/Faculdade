645329e933ef575793259c39be53b05c8b3d1238
Hallan Christian <hallanchristiancruz@gmail.com>
2024-03-19 12:30:42 -0300

    Add files via upload

---
+++ Java/1º Semestre/EstádioFutebol/src/app/Main.java
@@ -0,0 +1,35 @@
+/*
+Aluno: Hallan Christian Morais da Cruz
+Questão 01
+*/
+package app;
+
+import java.util.Scanner; 
+
+public class Main
+{
+	public static void main(String[] args) {
+	    
+	    Scanner sc = new Scanner (System.in);
+		
+		System.out.print("Digite a capacidade do estádio: ");
+		int capacidade = sc.nextInt();
+		
+		System.out.print("Digite a quantidade de pessoas: ");
+		int quantidade = sc.nextInt();
+		
+        if (quantidade >= 0.8 * capacidade) {
+            System.out.println("O estádio está lotado");
+        }
+        else if (quantidade >=0.5 * capacidade) {
+            System.out.println("O estádio está parcialmente lotado");
+        }
+        else if (quantidade < 0.5 * capacidade) {
+            System.out.println("O estádio está quase vazio");
+        }
+        else if (quantidade <= 0.2 * capacidade) {
+            System.out.println("O estádio está vazio");
+        }
+	}
+}
+
